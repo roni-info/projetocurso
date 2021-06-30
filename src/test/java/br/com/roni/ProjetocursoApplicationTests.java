036060ab2a8c038e55045e637fe410e68235ad8e
roni-info <roni_info@yahoo.com.br>
2021-06-30 19:32:40 -0300

    Projeto Inicial

---
+++ src/test/java/br/com/roni/ProjetocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
