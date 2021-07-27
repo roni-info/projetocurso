7794d21193424017d7117a04de695d66038a632b
roni-info <roni_info@yahoo.com.br>
2021-07-27 08:20:10 -0300

    aula 52

--- src/main/java/br/com/roni/model/Pedido.java
+++ src/main/java/br/com/roni/model/Pedido.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Pedido  implements Serializable{
+public class Pedido implements Serializable {
 
 	/**
 	 * 
@@ -30,11 +30,11 @@ public class Pedido  implements Serializable{
 	private Long id;
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
-	
+
 	@JsonManagedReference
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
 	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "id_cliente")
@@ -43,51 +43,62 @@ public class Pedido  implements Serializable{
 	public Pedido() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@ManyToOne
 	@JoinColumn(name = "id_endereco_de_entrega")
 	private Endereco enderecoDeEntrega;
 
 	@OneToMany(mappedBy = "id.pedido")
 	private Set<ItemPedido> itens = new HashSet<>();
-	
-	public Pedido(Long id, Date instante,  Cliente cliente, Endereco enderecoDeEntrega) {
+
+	public Pedido(Long id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
 		this.id = id;
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Date getInstante() {
 		return instante;
 	}
+
 	public void setInstante(Date instante) {
 		this.instante = instante;
 	}
+
 	public Pagamento getPagamento() {
 		return pagamento;
 	}
+
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
+
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,6 +106,7 @@ public class Pedido  implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -111,14 +123,21 @@ public class Pedido  implements Serializable{
 			return false;
 		return true;
 	}
+
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
+
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
-	
 
-	
-	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido itemPedido : itens) {
+			soma = soma + itemPedido.getSubTotal();
+		}
+		return soma;
+	}
+
 }
